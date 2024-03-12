2a02c313a0edc4ca67084df34fe1af27173791a1
BlueLlama_ <160927352+IBlueLlama@users.noreply.github.com>
2024-03-12 18:54:16 +0000

    new file:   img/hrm.png 	modified:   index.html 	modified:   main.js 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log("Hello World!")
+console.log("Hello World!");
 //$("Select Item").What happens(What happens pt.2)
-$("#Template").text("Template...")
-$("p").css("color", "red");
+$("#Template").text("Template...");
