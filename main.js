f1d5f405b263aef0804dd71fc05331fa5e0ab67a
BlueLlama_ <160927352+IBlueLlama@users.noreply.github.com>
2024-03-12 20:04:08 +0000

    new file:   img/Red.webp 	modified:   index.html 	modified:   main.js 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 console.log("Hello World!");
 //$("Select Item").What happens(What happens pt.2)
 $("#Template").text("Template...");
+// Main Menu Dropdown(Probably a better way to do this)
+$(".aboutMeBtn").click(function () { 
+    $(".whyMade").hide();
+    $(".whereTo").hide();
+    $(".aboutMe").toggle();
+    $(".aboutMeBtn").toggleClass("clickedBtn");
+    $(".whyMadeBtn").removeClass("clickedBtn");
+    $(".whereToBtn").removeClass("clickedBtn");
+});
+$(".whyMadeBtn").click(function () { 
+    $(".aboutMe").hide();
+    $(".whereTo").hide();
+    $(".whyMade").toggle();
+    $(".whyMadeBtn").toggleClass("clickedBtn");
+    $(".aboutMeBtn").removeClass("clickedBtn");
+    $(".whereToBtn").removeClass("clickedBtn");
+});
+$(".whereToBtn").click(function () { 
+    $(".aboutMe").hide();
+    $(".whyMade").hide();
+    $(".whereTo").toggle();
+    $(".whereToBtn").toggleClass("clickedBtn");
+    $(".aboutMeBtn").removeClass("clickedBtn");
+    $(".whyMadeBtn").removeClass("clickedBtn");
+});
+$("#sus").mouseenter (function () { 
+    $("#red").show();
+});
+$("#sus").mouseleave(function () { 
+    $("#red").hide();
+});
